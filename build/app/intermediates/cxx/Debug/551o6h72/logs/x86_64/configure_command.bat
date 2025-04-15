502f350ee0d84d8e2a8c81fdfc2b4ddf1292b0d1
Kwibels <leonardwinata092@gmail.com>
2025-04-15 20:05:48 +0700

    android running

---
+++ build/app/intermediates/cxx/Debug/551o6h72/logs/x86_64/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"C:\\Users\\yarig\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\yarig\\OneDrive\\Documents\\flutter\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\groovy" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86_64" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86_64" ^
+  "-DANDROID_NDK=C:\\Users\\yarig\\AppData\\Local\\Android\\sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\yarig\\AppData\\Local\\Android\\sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\yarig\\AppData\\Local\\Android\\sdk\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\yarig\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\yarig\\StudioProjects\\flutter_list\\ulinmahoniapps\\build\\app\\intermediates\\cxx\\Debug\\551o6h72\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\yarig\\StudioProjects\\flutter_list\\ulinmahoniapps\\build\\app\\intermediates\\cxx\\Debug\\551o6h72\\obj\\x86_64" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\yarig\\StudioProjects\\flutter_list\\ulinmahoniapps\\android\\app\\.cxx\\Debug\\551o6h72\\x86_64" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli
