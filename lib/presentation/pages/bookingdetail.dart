7a7685d1b7f2be5453bcf99026b4626c01e8f187
Kwibels <leonardwinata092@gmail.com>
2025-05-09 16:34:26 +0700

    add booking detail

---
+++ lib/presentation/pages/bookingdetail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Bookingdetail extends StatelessWidget {
+  const Bookingdetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
